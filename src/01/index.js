abc8dc124408f595f14f6b36c984f62b6cba0ab8
Ian Hansson <ian.hansson@gmail.com>
2020-12-21 08:03:35 +0000

    Excercise 1 without xstate

--- src/01/index.js
+++ src/01/index.js
@@ -1,25 +1,38 @@
+import { createMachine } from "xstate";
+
 const elBox = document.querySelector('#box');
+
+const machine = {
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        CLICK: 'active'
+      }
+    },
+    active: {
+      on: {
+        CLICK: 'inactive'
+      }
+    }
+  }
+}
 
 // Pure function that returns the next state,
 // given the current state and sent event
 function transition(state, event) {
-  switch (
-    state
-    // Add your state/event transitions here
-    // to determine and return the next state
-  ) {
-  }
+  return machine.states[state].on[event] || state;
 }
 
 // Keep track of your current state
-let currentState = undefined;
+let currentState = machine.initial;
 
 function send(event) {
-  // Determine the next value of `currentState`
-
+  currentState = transition(currentState, event)
   elBox.dataset.state = currentState;
 }
 
+send()
 elBox.addEventListener('click', () => {
-  // send a click event
+  send("CLICK")
 });
